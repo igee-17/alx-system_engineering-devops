ef6f82b367bad20e00e8fd20e811bd54c98dee13
igee <igbagboleye@gmail.com>
2022-09-13 18:01:28 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *main - Entry point
+ *Description: prints _putchar using putchar prototype
+ *
+ * Return: On success 1.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
